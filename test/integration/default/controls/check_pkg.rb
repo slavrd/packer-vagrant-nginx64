2c3e12adb2945c99aeb9e76dbd4b7bc325495422
Slav Donchev <slav.donchev@gmail.com>
2019-05-23 15:53:13 +0300

    remove nginx version check

--- test/integration/default/controls/check_pkg.rb
+++ test/integration/default/controls/check_pkg.rb
@@ -9,14 +9,4 @@ control 'nginx' do
     it {should be_running}
   end
 
-  nginx_ver = ENV['TEST_NGINX_VER']
-
-  if not [nil, '', 'auto'].include?(nginx_ver) then
-
-    describe command('nginx -v') do
-      its('stdout') { should match (/#{nginx_ver.split('-')[0]}/) }
-    end
-
-  end
-
 end
